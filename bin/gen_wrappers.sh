1ffd0a08add59ed3af0275dddc25dcf7284682b1
Chris Cheshire <chris.j.cheshire@gmail.com>
2023-02-20 13:05:44 +0000

    Whitespace

--- bin/gen_wrappers.sh
+++ bin/gen_wrappers.sh
@@ -5,6 +5,6 @@ PARAMS_FILE="./assets/module_list.txt"
 
 # Loop through each line in the text file
 while read -r PARAM; do
-  # Run the Python script with the current parameter
-  ./bin/create_wrapper.py --target "$PARAM"
+    # Run the Python script with the current parameter
+    ./bin/create_wrapper.py --target "$PARAM"
 done < "$PARAMS_FILE"
